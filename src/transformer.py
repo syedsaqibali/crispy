21630a7a042e22b652c21fa34f5d2317d82147b9
Saqib Ali <saqib.ali@bethesda.net>
2021-07-26 07:51:08 -0400

    Added comments

--- src/transformer.py
+++ src/transformer.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 
-import argparse
-from tqdm import tqdm
-import csv
-import yaml
-import re
-import subprocess
-import datetime
+import argparse             # To parse command-line arguments
+from tqdm import tqdm       # To draw a nifty progress-bar. From here: https://github.com/tqdm/tqdm
+import csv                  # To read & write CSV files
+import yaml                 # To parse YAML configuration file
+import re                   # To use regular expressions
+import subprocess           # To make a system call
+import datetime             # To manipulate dates. Not used in the code but required by the configuration file
 
+# Define the valid data types used by the configuration file
 DATA_TYPES = {
     'INTEGER',
     'FLOAT',
     'STRING'
 }
 
+# Define what a variable will look like in the configuration file
 VARIABLE_REGEXP = re.compile('\$\{[A-Za-z0-9 ]+\}')
 
+
+# Define the interface for Inputters
 class InputInterface:
     def __init__(self, source, transforms):
         raise NotImplementedError
@@ -30,19 +34,17 @@ class InputInterface:
     def __iter__(self):
         return self
 
-
+# Define the interface for Outputters
 class OutputInterface:
     def __init__(self, target, transforms):
         raise NotImplementedError
     def put_row(self, data):
         raise NotImplementedError
-    def get_current_row_num(self):
-        raise NotImplementedError
     def tear_down(self):
         raise NotImplementedError
 
-
-class FileInput(InputInterface):
+# Define a class that implements the InputInterface for reading from a CSV file
+class CSVFileInput(InputInterface):
     def __init__(self, source, transforms):
         self.source = source
         self.transforms = transforms
@@ -51,6 +53,10 @@ class FileInput(InputInterface):
         self.open_file = open(self.source, newline='')
         self.reader = csv.DictReader(self.open_file)
 
+    # The iterator functionality that gets called when an instance of this class
+    # is looped over. It reads a row of data from the input file, transforms
+    # the fields to the specified type and returns the data. Any malformed
+    # data that cannot be transformed causes an exception to be thrown
     def __next__(self):
         file_row = self.reader.__next__()
         self.current_row_num += 1
@@ -67,15 +73,19 @@ class FileInput(InputInterface):
                 assert False, 'It should never get here!'
         return transformed_row
 
+    # Report the total number of rows of data in this file
     def num_total_rows(self):
         return self.total_rows
 
+    # Report the current row number
     def get_current_row_num(self):
         return self.current_row_num
 
+    # Clean Up
     def tear_down(self):
         self.open_file.close()
 
+    # Calculate the total number of rows of data in this file
     def _num_rows_in_file(self):
         p = subprocess.Popen(['wc', '-l', self.source],
                              stdout=subprocess.PIPE,
@@ -86,25 +96,29 @@ class FileInput(InputInterface):
         return int(result.strip().split()[0])
 
 
-class FileOutput(OutputInterface):
+# Define a class that implements the OutputInterface for writing to a CSV file
+class CSVFileOutput(OutputInterface):
     def __init__(self, target, transforms):
         self.target = target
         self.transforms = transforms
-        self.current_row_num = 0
         self.open_file = open(self.target, 'w')
         self.writer = csv.DictWriter(self.open_file, self.transforms.keys())
         self.writer.writeheader()
 
+    # Substitue the (possibly multiple occurrences) of variables in the
+    # transform with their actual values from data
     def _substitute_variables(self, transform_string, data):
         replacement_set = set(VARIABLE_REGEXP.findall(transform_string))
         for i in replacement_set:
+            # Remove the leading ${ and the trailing } so we have
+            # the substitution variable name's
             source_field_name = i[2:-1]
             transform_string = transform_string.replace(i, str(data[source_field_name]))
         return transform_string
 
+    # Take a record, transform it according to the configuration and output it
     def put_row(self, data):
         transformed_row = {}
-        self.current_row_num += 1
         for field in self.transforms.keys():
             transformed_value = eval(self._substitute_variables(transform_string=self.transforms[field]['Transform'],
                                                                 data=data))
@@ -119,12 +133,11 @@ class FileOutput(OutputInterface):
                 assert False, 'It should never get here!'
         self.writer.writerow(transformed_row)
 
-    def get_current_row_num(self):
-        return self.current_row_num
-
+    # Clean up
     def tear_down(self):
         self.open_file.close()
 
+# Parse and validate the configuration file. If it is malformed throw an exception
 def parse_config_yaml(config_file):
 
     # Parse the configuration file
@@ -144,29 +157,24 @@ def parse_config_yaml(config_file):
 
     return parsed_config
 
-
+# The Main function
 def main():
+    # Parse the command-line arguments
     parser = argparse.ArgumentParser()
-
     requiredNamed = parser.add_argument_group('required named arguments')
-    requiredNamed.add_argument('-c', '--config',
-                               required=True,
-                               help='Configuration specification file')
-    requiredNamed.add_argument('-i', '--input',
-                               required=True,
-                               help='Input CSV file')
-    requiredNamed.add_argument('-s', '--success',
-                               required=True,
-                               help='Output success file')
-    requiredNamed.add_argument('-f', '--failure',
-                               required=True,
-                               help='Output failure file')
+    requiredNamed.add_argument('-c', '--config', required=True, help='Configuration specification file')
+    requiredNamed.add_argument('-i', '--input', required=True, help='Input CSV file')
+    requiredNamed.add_argument('-s', '--success', required=True, help='Output success file')
+    requiredNamed.add_argument('-f', '--failure', required=True, help='Output failure file')
     args = parser.parse_args()
 
+    # Get the configuration
     config_spec = parse_config_yaml(config_file=args.config)
 
-    inputter = FileInput(source=args.input, transforms=config_spec['input-fields'])
-    outputter = FileOutput(target=args.success, transforms=config_spec['output-fields'])
+    # Go through the Input CSVfile, transforming the data according to the configuration file and
+    # and output the transformed row to a CSV file
+    inputter = CSVFileInput(source=args.input, transforms=config_spec['input-fields'])
+    outputter = CSVFileOutput(target=args.success, transforms=config_spec['output-fields'])
     for row in tqdm(inputter, total=inputter.num_total_rows()):
         outputter.put_row(row)
     outputter.tear_down()
